3ac1c56816b6069a6c98d2f0131a89d9f72b2b43
Maryam Tahir <maryamtah8@gmail.com>
2021-02-19 14:20:52 -0500

    Save all

---
+++ controllers/burgersController.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+const burgers = require('../models/burgers.js');
+
+
+router.get('/', (req, res) => {
+  burgers.all((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+router.post('/api/burgers', (req, res) => {
+  burgers.create(['burger_name', 'devoured'], [req.body.name, req.body.devoured], (result) => {    res.json({ id: result.insertId });
+  });
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log('condition', condition);
+
+  burgers.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+router.delete('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  burgers.delete(condition, (result) => {
+    if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
